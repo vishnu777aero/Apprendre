2b022b1cded2e4e209d10790027700881a565cf9
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-05-05 01:31:30 -0400

    add translation functionality
    use translation api from local server
    to translation selected text between English and French

--- scripts/content.js
+++ scripts/content.js
@@ -5,7 +5,7 @@ const tooltip = document.createElement('div');
 tooltip.id = tooltipId;
 document.body.appendChild(tooltip);
 
-document.addEventListener('mouseup', (event) => {
+document.addEventListener('mouseup', async (event) => {
     const selectedText = window.getSelection().toString().trim();
     
     if(selectedText !== '') {
@@ -13,10 +13,15 @@ document.addEventListener('mouseup', (event) => {
         const mouseY = event.pageY;
         
         const tooltip = document.getElementById(tooltipId);
-        tooltip.innerText = selectedText;
+
         tooltip.style.top = mouseY + 'px';
         tooltip.style.left = mouseX + 'px';
         tooltip.style.display = 'block';
+
+        const detectionResponse = await detectLanguage(selectedText);
+        const targetLanguage = findTargetLanguage({ detectedLanguage: detectionResponse[0]?.language });
+        const translationResponse = await translate({ text: selectedText, target: targetLanguage });
+        tooltip.innerText = translationResponse.translatedText;
     }
 });
 
@@ -26,3 +31,52 @@ document.addEventListener('mousedown', (event) => {
         tooltip.style.display = 'none';
     }
 });
+
+const detectLanguage = async (text) => {
+    return fetch("http://localhost:5000/detect", {
+	    method: "POST",
+	    body: JSON.stringify({
+		q: text,
+		api_key: ""
+	    }),
+	    headers: { "Content-Type": "application/json" }
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("translation error")
+        }
+    }).catch((e) => {
+        console.log(e);
+    });
+}
+
+const translate = async ({ text, target }) => {
+    return fetch("http://localhost:5000/translate", {
+	method: "POST",
+	body: JSON.stringify({
+		q: text,
+		source: "auto",
+		target,
+		format: "text",
+		api_key: ""
+	}),
+	headers: { "Content-Type": "application/json" }
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("translation error")
+        }
+    }).catch((e) => {
+        console.log(e);
+    });
+}
+
+const findTargetLanguage = ({ detectedLanguage }) => {
+    switch (detectedLanguage) {
+        case "en": return "fr";
+        case "fr": return "en";
+        default: return "en";
+    }
+}
