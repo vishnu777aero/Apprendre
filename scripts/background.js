ecce3328622478e6485c3d3479e1d21ecc4188d3
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-05-05 23:39:32 -0400

    fix warnings

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,3 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete') {
-        chrome.tabs.insertCSS(tabId, {file: 'css/tooltip.css'});
-    }
-});
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === 'speak') {
         chrome.tts.speak(request.text, { lang: request.lang });
