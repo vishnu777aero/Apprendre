a6686a8168ce577cf4eeca81ac1ec9e4d424edf8
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-05-06 22:56:57 -0400

    restructure code

--- scripts/content.js
+++ scripts/content.js
@@ -25,10 +25,10 @@ document.addEventListener('mouseup', async (event) => {
         tooltip.style.left = mouseX + 'px';
         tooltip.style.display = 'block';
 
-        const detectionResponse = await detectLanguage(selectedText);
+        const detectionResponse = await detectLanguageApi(selectedText);
         state.targetLanguage = findTargetLanguage({ detectedLanguage: detectionResponse[0]?.language });
         const { targetLanguage } = state;
-        const { translatedText } = await translate({ text: selectedText, target: targetLanguage });
+        const { translatedText } = await translateApi({ text: selectedText, target: targetLanguage });
         state.translatedText = translatedText;
         tooltip.innerText = translatedText;
     }
@@ -40,47 +40,6 @@ document.addEventListener('mousedown', (event) => {
         tooltip.style.display = 'none';
     }
 });
-
-const detectLanguage = async (text) => {
-    return fetch("http://localhost:5000/detect", {
-	    method: "POST",
-	    body: JSON.stringify({
-		q: text,
-		api_key: ""
-	    }),
-	    headers: { "Content-Type": "application/json" }
-    }).then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("translation error")
-        }
-    }).catch((e) => {
-        console.log(e);
-    });
-}
-
-const translate = async ({ text, target }) => {
-    return fetch("http://localhost:5000/translate", {
-	method: "POST",
-	body: JSON.stringify({
-		q: text,
-		source: "auto",
-		target,
-		format: "text",
-		api_key: ""
-	}),
-	headers: { "Content-Type": "application/json" }
-    }).then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("translation error")
-        }
-    }).catch((e) => {
-        console.log(e);
-    });
-}
 
 const findTargetLanguage = ({ detectedLanguage }) => {
     switch (detectedLanguage) {
